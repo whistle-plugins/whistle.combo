0de1a28ae36cdc302affc04e92cbbd350c4cb921
jwlin <eagleking0213@gmail.com>
2017-02-26 15:47:53 +0800

    0.0.1

--- middleware/proxy.js
+++ middleware/proxy.js
@@ -12,13 +12,3 @@ module.exports = (root) => {
     }
   };
 };
-
-// function test() {
-//   const root = '/Users/jiewei.ljw/work/fe';
-//   const parts = '/aefe-mobile-lego/standalone/lego-dialog/'.split('/');
-//   let dir = path.join(root, parts[1]);
-//   console.log(dir)
-//   return require('fs').exists(dir);
-// }
-// console.log(test());
-
